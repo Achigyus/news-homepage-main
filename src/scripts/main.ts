a8edbde16b171d40e280a258deeaedaf31d1ec6a
Achigyus <gyuseachi@yahoo.com>
2025-04-30 22:20:28 +0100

    start building logic

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,2 +1,30 @@
 import '../sass/index.sass'
 
+let menuBtn = document.getElementById('menu_btn') as HTMLButtonElement
+let menuOpenState = false
+
+
+function toggleMenu() {
+    if (menuBtn) {
+        console.log(
+            'Menu button clicked. Current state: ' + menuOpenState + '. Toggling...'
+        )
+        let openImg = menuBtn.querySelector('img.open') as HTMLImageElement
+        let closeImg = menuBtn.querySelector('img.close') as HTMLImageElement
+        let menu = document.getElementById('nav_links_ul') as HTMLUListElement
+
+        if (menuOpenState) {
+            openImg.classList.add('hidden')
+            closeImg.classList.remove('hidden')
+            menu.classList.remove('hidden')
+            menuOpenState = false
+        } else {
+            openImg.classList.remove('hidden')
+            closeImg.classList.add('hidden')
+            menu.classList.add('hidden')
+            menuOpenState = true
+        }
+    }
+}
+
+menuBtn.addEventListener('click', toggleMenu)
