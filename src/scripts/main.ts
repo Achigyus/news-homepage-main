f5f72adfe0a674084a6db4a5ba25c3a7a9580d52
Achigyus <gyuseachi@yahoo.com>
2025-05-01 19:47:15 +0100

    done with js logic

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -12,19 +12,21 @@ function toggleMenu() {
         let openImg = menuBtn.querySelector('img.open') as HTMLImageElement
         let closeImg = menuBtn.querySelector('img.close') as HTMLImageElement
         let menu = document.getElementById('nav_links_ul') as HTMLUListElement
-
-        if (menuOpenState) {
-            openImg.classList.add('hidden')
-            closeImg.classList.remove('hidden')
-            menu.classList.remove('hidden')
-            menuOpenState = false
-        } else {
-            openImg.classList.remove('hidden')
-            closeImg.classList.add('hidden')
-            menu.classList.add('hidden')
-            menuOpenState = true
+        if (!openImg || !closeImg || !menu) {
+            console.error('Menu button or images not found')
+            return
         }
+        updateDOM(openImg, closeImg, menu, !menuOpenState)
     }
+}
+
+function updateDOM(open: HTMLImageElement, close: HTMLImageElement, menu: HTMLUListElement, state: boolean) {
+    open.classList.toggle('hidden', state)
+    close.classList.toggle('hidden', !state)
+    menu.classList.toggle('active', state)
+    menuOpenState = state
+    menuBtn.setAttribute('aria-expanded', `${state}`)
+
 }
 
 menuBtn.addEventListener('click', toggleMenu)
