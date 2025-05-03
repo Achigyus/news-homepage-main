3a4f53e73f3c0f9d4cbff26cb94b84c0cefb683f
Achigyus <gyuseachi@yahoo.com>
2025-05-03 13:24:40 +0100

    finish mobile styles

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,9 @@
 import '../sass/index.sass'
 
 let menuBtn = document.getElementById('menu_btn') as HTMLButtonElement
+let overlay = document.getElementById('overlay') as HTMLDivElement
 let menuOpenState = false
+let menuLinks = document.querySelectorAll('#nav_links_ul li a') as NodeListOf<HTMLAnchorElement>
 
 
 function toggleMenu() {
@@ -16,17 +18,28 @@ function toggleMenu() {
             console.error('Menu button or images not found')
             return
         }
-        updateDOM(openImg, closeImg, menu, !menuOpenState)
+        updateDOM(openImg, closeImg, menu, overlay, !menuOpenState)
     }
 }
 
-function updateDOM(open: HTMLImageElement, close: HTMLImageElement, menu: HTMLUListElement, state: boolean) {
+function updateDOM(open: HTMLImageElement, close: HTMLImageElement, menu: HTMLUListElement, overlay: HTMLDivElement, state: boolean) {
     open.classList.toggle('hidden', state)
     close.classList.toggle('hidden', !state)
     menu.classList.toggle('active', state)
+    overlay.classList.toggle('active', state)
     menuOpenState = state
     menuBtn.setAttribute('aria-expanded', `${state}`)
 
 }
 
 menuBtn.addEventListener('click', toggleMenu)
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        if (menuOpenState) {
+            let openImg = menuBtn.querySelector('img.open') as HTMLImageElement
+            let closeImg = menuBtn.querySelector('img.close') as HTMLImageElement
+            let menu = document.getElementById('nav_links_ul') as HTMLUListElement
+            updateDOM(openImg, closeImg, menu, overlay, false)
+        }
+    })
+})
